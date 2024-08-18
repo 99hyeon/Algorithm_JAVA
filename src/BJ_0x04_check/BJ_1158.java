991282e416bd071cc8a3f0b79cbc20621dbab550
99hyeon <hyeon99@ajou.ac.kr>
2024-08-18 19:34:28 +0900

    1158-5397 solved 2 problem(0x05 finish)

---
+++ src/BJ_0x04_check/BJ_1158.java
@@ -0,0 +1,46 @@
+package BJ_0x04_check;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class BJ_1158 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        LinkedList<Integer> people = new LinkedList<>();
+
+        for(int i = 0; i < N; i++){
+            people.add(i+1);
+        }
+
+        int order = K-1;
+
+        LinkedList<Integer> tmp = new LinkedList<>();
+        while(people.size() > 0){
+            if(order < people.size()){
+                tmp.add(people.get(order));
+                people.remove(order);
+                order += (K-1);
+            }
+            else if(order >= people.size()){
+                order -= people.size();
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        for(int i = 0; i < tmp.size(); i++){
+            if(i == tmp.size()-1){
+                sb.append(tmp.get(i));
+            }else{
+                sb.append(tmp.get(i)).append(",").append(" ");
+            }
+        }
+        sb.append(">");
+
+        System.out.println(sb);
+    }
+}
---
+++ src/BJ_0x04_check/BJ_5397.java
@@ -0,0 +1,65 @@
+package BJ_0x04_check;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class BJ_5397 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        LinkedList<Character> pw;
+        for(int i = 0; i < num; i++){
+            String tmpPw = br.readLine();
+
+            pw = new LinkedList<>();
+            int index = 0;
+            for(int j = 0; j < tmpPw.length(); j++){
+                char tmp = tmpPw.charAt(j);
+
+                switch (tmp){
+                    case '>':
+                        if(index == pw.size()){
+                            break;
+                        }else{
+                            index++;
+                            break;
+                        }
+                    case '<':
+                        if(index == 0){
+                            break;
+                        }else{
+                            index--;
+                            break;
+                        }
+                    case '-':
+                        if(index == 0){
+                            break;
+                        }else{
+                            pw.remove(--index);
+                            break;
+                        }
+                    default:
+                        pw.add(index, tmp);
+                        index++;
+                        break;
+                }
+
+            }
+
+            for (Character character : pw) {
+                sb.append(character);
+            }
+            sb.append("\n");
+
+        }
+
+        System.out.println(sb);
+    }
+}
