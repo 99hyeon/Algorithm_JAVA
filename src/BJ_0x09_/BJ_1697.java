c8e6f88174d63fd08dbec22b698ec3c2ab69c6da
99hyeon <hyeon99@ajou.ac.kr>
2024-08-31 12:52:48 +0900

    1697 solved 1 problem

---
+++ src/BJ_0x09_/BJ_1697.java
@@ -0,0 +1,59 @@
+package BJ_0x09_;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BJ_1697 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] seconds = new int[200001];
+        for(int i = 0; i < seconds.length; i++){
+            seconds[i] = -1;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(N);
+        seconds[N] = 0;
+
+        while(!queue.isEmpty()){
+            int tmp = queue.poll();
+
+            int[] dx = {-1, 1, 2};
+            int next = tmp;
+
+            boolean check = false;
+            for(int i = 0; i < 3; i++){
+                if(i == 2){
+                    next = tmp*dx[i];
+                }else
+                    next = tmp + dx[i];
+
+                if(next < 0 || next >= seconds.length)
+                    continue;
+
+                if(seconds[next] != -1)
+                    continue;
+
+                else{
+                    seconds[next] = seconds[tmp] + 1;
+                    queue.add(next);
+
+                    if(next == K){
+                        check = true;
+                        break;
+                    }
+                }
+            }
+
+            if(check)
+                break;
+        }
+
+        System.out.println(seconds[K]);
+    }
+}
