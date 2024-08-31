e4a8b0c6de87ae00c4b59262e2316002ee073592
99hyeon <hyeon99@ajou.ac.kr>
2024-08-31 22:01:16 +0900

    1012-10026 solved 2 problems

---
+++ src/BJ_0x09_/BJ_10026.java
@@ -0,0 +1,105 @@
+package BJ_0x09_;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BJ_10026 {
+
+    static int n;
+    static char[][] board;
+    static boolean[][] visit;
+    static boolean[][] visitBlind;
+
+    static int area = 0;
+    static int areaBlind = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+
+        board = new char[n][n];
+        visit = new boolean[n][n];
+        visitBlind = new boolean[n][n];
+        for(int i = 0; i < n; i++){
+            String tmp = br.readLine();
+            for(int j = 0; j < n; j++){
+                board[i][j] = tmp.charAt(j);
+            }
+        }
+        br.close();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(!visit[i][j]){
+                    bfs(i, j, board[i][j], false);
+                }
+                if(!visitBlind[i][j]){
+                    bfs(i, j, board[i][j], true);
+                }
+            }
+        }
+
+        System.out.println(area + " " + areaBlind);
+    }
+
+    static void bfs(int x, int y, char color, boolean blindness){
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {x, y});
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int currentX = current[0];
+            int currentY = current[1];
+
+            for(int i = 0; i < 4; i++){
+                int nextX = currentX + dx[i];
+                int nextY = currentY + dy[i];
+
+                if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= n)
+                    continue;
+
+                if(blindness){
+                    if(color == 'R' || color == 'G'){
+                        if(board[nextX][nextY] != 'B' && !visitBlind[nextX][nextY]){
+                            queue.add(new int[] {nextX, nextY});
+                            visitBlind[nextX][nextY] = true;
+                        }
+                    }else{
+                        if(board[nextX][nextY] == 'B' && !visitBlind[nextX][nextY]){
+                            queue.add(new int[] {nextX, nextY});
+                            visitBlind[nextX][nextY] = true;
+                        }
+                    }
+                }else{
+                    if(color == 'R' && board[nextX][nextY] == 'R' && !visit[nextX][nextY]){
+                        queue.add(new int[] {nextX, nextY});
+                        visit[nextX][nextY] = true;
+                    }else if(color == 'G' && board[nextX][nextY] == 'G' && !visit[nextX][nextY]){
+                        queue.add(new int[] {nextX, nextY});
+                        visit[nextX][nextY] = true;
+                    }else if(color == 'B' && board[nextX][nextY] == 'B' && !visit[nextX][nextY]){
+                        queue.add(new int[] {nextX, nextY});
+                        visit[nextX][nextY] = true;
+                    }
+                }
+            }
+        }
+        if(blindness){
+            areaBlind++;
+        }else{
+            area++;
+        }
+    }
+}
+
+/**
+ * 적록색약 : (R G) B
+ * 적록색약X : R G B
+ */
---
+++ src/BJ_0x09_/BJ_1012.java
@@ -0,0 +1,83 @@
+package BJ_0x09_;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BJ_1012 {
+
+    static int[][] board;
+    static int width;
+    static int height;
+    static boolean[][] visited;
+    static int earthworm;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int testNum = Integer.parseInt(br.readLine());
+        for(int i = 0; i < testNum; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            width = Integer.parseInt(st.nextToken());
+            height = Integer.parseInt(st.nextToken());
+            int vegetable = Integer.parseInt(st.nextToken());
+
+            board = new int[width][height];
+            for(int j = 0; j < vegetable; j++){     //배추 세팅
+                st = new StringTokenizer(br.readLine());
+
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+
+                board[x][y] = 1;
+            }
+
+            visited = new boolean[width][height];
+            earthworm = 0;
+            for(int j = 0; j < width; j++){
+                for(int k = 0; k < height; k++){
+                    if(board[j][k] == 1 && !visited[j][k]){
+                        bfs(j, k);
+                        earthworm++;
+                    }
+                }
+            }
+            sb.append(earthworm).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static void bfs(int x, int y){
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {x, y});
+
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int currentX = current[0];
+            int currentY = current[1];
+
+            for(int i = 0; i < 4; i++){
+                int nextX = currentX + dx[i];
+                int nextY = currentY + dy[i];
+
+                if(nextX < 0 || nextY < 0 || nextX >= width || nextY >= height)
+                    continue;
+                if(board[nextX][nextY] == 0 || visited[nextX][nextY])
+                    continue;
+
+                queue.add(new int[] {nextX, nextY});
+                visited[nextX][nextY] = true;
+            }
+        }
+    }
+}
