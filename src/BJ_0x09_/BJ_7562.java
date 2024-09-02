660ae7eb1d78f1ce4acfb859e2e144946bd76cb5
99hyeon <hyeon99@ajou.ac.kr>
2024-09-02 10:52:27 +0900

    7562 solved 1 problem

---
+++ src/BJ_0x09_/BJ_7562.java
@@ -0,0 +1,75 @@
+package BJ_0x09_;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BJ_7562 {
+
+    static int[][] board;
+    static boolean[][] visit;
+    static int size;
+    static int[] start;
+    static int[] target;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int testNum = Integer.parseInt(br.readLine());
+        for(int i = 0; i < testNum; i++){
+            size = Integer.parseInt(br.readLine());
+            board = new int[size][size];
+
+            for(int j = 0; j < 2; j++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+
+                if(j == 0){
+                    bfs(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+                }else{
+                    sb.append(board[Integer.parseInt(st.nextToken())][Integer.parseInt(st.nextToken())]).append("\n");
+                }
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static void bfs(int x, int y){
+        int[] dx = {-2, -2, 2, 2, -1, 1, -1, 1};
+        int[] dy = {-1, 1, -1, 1, -2, -2, 2, 2};
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {x, y});
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            int currentX = current[0];
+            int currentY = current[1];
+
+            for(int i = 0; i < 8; i++){
+                int nextX = currentX + dx[i];
+                int nextY = currentY + dy[i];
+
+                if(nextX == x && nextY == y)
+                    continue;
+                if(nextX < 0 || nextY < 0 || nextX >= size || nextY >= size)
+                    continue;
+
+                if(board[nextX][nextY] == 0){
+                    board[nextX][nextY] = board[currentX][currentY] + 1;
+                    queue.add(new int[] {nextX, nextY});
+                }else if(board[nextX][nextY] > board[currentX][currentY] + 1){
+                    board[nextX][nextY] = board[currentX][currentY] + 1;
+                    queue.add(new int[] {nextX, nextY});
+                }
+            }
+        }
+
+    }
+
+
+}
