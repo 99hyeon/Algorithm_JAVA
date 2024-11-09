0eeb203012d08b22d8eb9c016e3672b6f9acd161
99hyeon <hyeon99@ajou.ac.kr>
2024-11-09 13:06:31 +0900

    10807, 13300 solved 2 problem(0x03)

---
+++ src/BJ_0x03_check/BJ_13300.java
@@ -0,0 +1,40 @@
+package BJ_0x03_check;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_13300 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int studentNum = Integer.parseInt(st.nextToken());
+        int maxNum = Integer.parseInt(st.nextToken());
+        int roomCnt = 0;
+
+        int[][] gradeSex = new int[6][2];
+        for(int i = 0; i < studentNum; i++){
+            st = new StringTokenizer(br.readLine());
+            int sex = Integer.parseInt(st.nextToken());
+            int grade = Integer.parseInt(st.nextToken());
+
+            gradeSex[grade - 1][sex]++;
+        }
+
+        for (int[] sex : gradeSex) {
+            for (int i : sex) {
+                if(i != 0){
+                    if(i % maxNum != 0){
+                        roomCnt += (i / maxNum) + 1;
+                    }else{
+                        roomCnt += i / maxNum;
+                    }
+                }
+            }
+        }
+
+        System.out.println(roomCnt);
+    }
+}
