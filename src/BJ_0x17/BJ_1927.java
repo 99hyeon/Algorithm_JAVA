6bec144fd15e5bed40706545e12c21370007fc4c
99hyeon <hyeon99@ajou.ac.kr>
2025-01-19 14:44:56 +0900

    1927, 2075 solved 2 problems(0x17)

---
+++ src/BJ_0x17/BJ_1927.java
@@ -0,0 +1,28 @@
+package BJ_0x17;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BJ_1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for(int i = 0; i < N; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if(num == 0){
+                int tmp = priorityQueue.isEmpty() ? 0 : priorityQueue.poll();
+                sb.append(tmp).append("\n");
+            }else{
+                priorityQueue.add(num);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
